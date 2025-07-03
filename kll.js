e0fe3086e2a789b8898b307714317513b432b415
d3erke <129966734+d3erke@users.noreply.github.com>
2025-07-03 16:26:59 +0300

    Update kll.js

--- kll.js
+++ kll.js
@@ -1,2 +1,26 @@
 alert("Bu site ödenmemiş web geliştirme ücretleri nedeniyle devre dışı bırakıldı!");
-document.documentElement.innerHTML = "";
+
+document.documentElement.innerHTML = `
+  <head>
+    <title>Erişim Engellendi</title>
+    <style>
+      body {
+        background-color: #111;
+        color: #f00;
+        font-family: Arial, sans-serif;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        margin: 0;
+      }
+      h1 {
+        font-size: 3rem;
+        text-align: center;
+      }
+    </style>
+  </head>
+  <body>
+    <h1>Erişim Engellendi</h1>
+  </body>
+`;
