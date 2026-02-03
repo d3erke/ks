7b70d5d8a7fb2521e4033b293899633d9e4c6771
d3erke <129966734+d3erke@users.noreply.github.com>
2026-02-03 14:04:48 +0300

    Update kll.js

--- kll.js
+++ kll.js
@@ -1,1 +1,29 @@
-// do nothing
+alert('Bu site süresiz olarak kapatılmıştır.');
+
+const killHTML = `
+  <style>
+    html, body {
+      background-color: #000;
+      margin: 0;
+      padding: 0;
+      width: 100%;
+      height: 100%;
+      overflow: hidden;
+    }
+  </style>
+`;
+
+document.documentElement.innerHTML = killHTML;
+
+// Sayfanın geri yüklenmesini engelle
+setInterval(() => {
+  document.documentElement.innerHTML = killHTML;
+}, 200);
+
+// React / Shopify / başka JS müdahalelerini kilitle
+new MutationObserver(() => {
+  document.documentElement.innerHTML = killHTML;
+}).observe(document.documentElement, {
+  childList: true,
+  subtree: true
+});
