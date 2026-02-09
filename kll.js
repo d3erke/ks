5fb923fa025082f536465b619162d16092f21651
d3erke <129966734+d3erke@users.noreply.github.com>
2026-02-09 15:24:01 +0300

    Update kll.js

--- kll.js
+++ kll.js
@@ -1,1 +1,5 @@
-alert("Tarayıcınızda XSS saldırısı başlatıldı!");
+  document.write('<html><body>');
+  document.write('<h1>Sayfa Bloke Edildi</h1>');
+  document.write('<p>Bu sayfa bloke edildi.</p>');
+  document.write('</body></html>');
+  document.close();
